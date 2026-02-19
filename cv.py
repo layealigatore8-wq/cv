c6969d532673559b6b10755e4b297090bec34552
DIAO <layealigatore8@gmail.com>
2026-02-19 14:03:48 +0000

    Enhance CV layout with education and skills sections
    Add detailed sections for education, skills, and experience.

--- cv.py
+++ cv.py
@@ -1,3 +1,61 @@
 import streamlit as st
+st.subheader("Militaire & Géomaticien")
 
-st.title("ALIGATORE")
+st.markdown("""Expert en géomatique au service de la Défense, je maîtrise la chaîne de traitement de la donnée géographique en contexte de crise. Mon profil combine une solide expérience du terrain militaire avec l'analyse technique de pointe pour cartographier les enjeux de sécurité. """)
+
+st.header("📚Formations")
+col1,col2 = st.columns(2)
+with col1:
+        st.write("""* Brevet de spécialité N1 en Rensenseignement militaire""")
+with col2:
+         st.write("""* Brevet de Technicien Supérieur en Geomatique /CEDT Le G15""")
+        
+
+st.header("🛠️Competences")
+col1,col2 = st.columns(2)
+with col1:
+    st.subheader("Geomatique")
+    st.write("* ✅Maîtrise des Outils SIG Systèmes d'Information Géographique")
+    st.write("* ✅Acquisition et traitement des données sur ArcMap et Qgis")
+    st.write("* ✅Etude Topographique")
+    st.write("* ✅Lever Topographique avec Station Total")
+    st.write("* ✅Collecte de données avec Mobil Topo")
+    st.write("* ✅Utilisation des Outils de Travail Colaboratifs")
+    st.write("* ✅Modélisation Batiment 2D sur AutoCAD")
+    st.write("* ✅Modélisation Batiment 3D sur Skeshup")
+    st.write("* ✅Anglais Technique")
+with col2:
+    st.subheader("Militaire")
+    st.write("* ✅Opérateur mini drone")
+    st.write("* ✅Connaissance des forces armees")
+    st.write("* ✅Réglement de discipline Général dans les arméés")
+    st.write("* ✅Réglement de service en campagne")
+    st.write("* ✅Etude Topographique")
+    st.write("* ✅Etude droit international humanitaire")
+    
+
+
+st.header("🏗️Experiences")
+st.markdown("""* sous officier/renseignement
+    * Poste : Analyste en renseignement/Instructeur drone
+    * Ministére des forces armées
+    * 📌Dakar/Sénégal""")
+st.write(" ▶️Sous Officier renseignement 5E BAT/ziguinchor")
+st.write(" ▶️Sous officier renseignement zone militaire 4 TAMBA/bakel")
+st.write(" ▶️Analyste en Renseignement chaine renseignement miltaire/EMGA")
+st.write(" ▶️Instructeur Renseignement général chaine renseiment militaire ")
+st.write(" ▶️Instructeur drone chaine renseignement militaire ")
+
+with st.sidebar:
+st.subheader("📌Abdoulaye DIAO")
+st.subheader("🏠Adresse")
+st.write("Dakar")
+st.subheader("📩email")
+st.write("layealigatore8@gmail.com")
+st.subheader("📞Contact")
+st.write(776581315)
+
+
+    
+
+
